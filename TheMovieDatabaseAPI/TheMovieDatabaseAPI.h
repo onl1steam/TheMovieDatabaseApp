087fc528397b052b14452e74700ed77b47a9713e
Artem Ryzhkov (intern) <onl1steam@gmail.com>
2020-03-13 22:48:10 +0300

    Добавлен таргет фреймворк для сетевого слоя

---
+++ TheMovieDatabaseAPI/TheMovieDatabaseAPI.h
@@ -0,0 +1,19 @@
+//
+//  TheMovieDatabaseAPI.h
+//  TheMovieDatabaseAPI
+//
+//  Created by Рыжков Артем on 13.03.2020.
+//  Copyright © 2020 Рыжков Артем. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheMovieDatabaseAPI.
+FOUNDATION_EXPORT double TheMovieDatabaseAPIVersionNumber;
+
+//! Project version string for TheMovieDatabaseAPI.
+FOUNDATION_EXPORT const unsigned char TheMovieDatabaseAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheMovieDatabaseAPI/PublicHeader.h>
+
+
